b08f2ac5a150d95ffeaa56481d04cf7b86d44451
Beaudette <dylan.beaudette@ca.usda.gov>
2022-09-08 09:44:32 -0700

    tiny changes, will have to perform an complete overhaul with new software configuration

--- process-DEM-GRASS.R
+++ process-DEM-GRASS.R
@@ -5,8 +5,8 @@ library(link2GI)
 library(rgrass7)
 
 # for now, force rgrass7 to use the sp-interface
-library(sp)
-use_sp()
+# library(sp)
+# use_sp()
 
 library(rasterVis)
 library(viridisLite)
@@ -23,7 +23,7 @@ library(RColorBrewer)
 # ideas from: https://geocompr.robinlovelace.net/gis.html#rgrass
 
 # kind of slow
-gr <- findGRASS() 
+gr <- findGRASS()
 
 # find a GRASS 7 installation, and use the first one
 ind <- grep("7", gr$version)[1]
@@ -31,12 +31,12 @@ ind <- grep("7", gr$version)[1]
 # next line of code only necessary if we want to use GRASS as installed by 
 # OSGeo4W. Among others, this adds some paths to PATH, which are also needed
 # for running GRASS.
-link2GI::paramGRASSw(gr[ind, ])
+# link2GI::paramGRASSw(gr[ind, ])
 
-grass_path <- ifelse(test = !is.null(gr$installation_type) && 
-                       gr$installation_type[ind] == "osgeo4W",
-                     yes = file.path(gr$instDir[ind], "apps/grass", gr$version[ind]),
-                     no = gr$instDir)
+# grass_path <- ifelse(test = !is.null(gr$installation_type) &&
+#                        gr$installation_type[ind] == "osgeo4W",
+#                      yes = file.path(gr$instDir[ind], "apps/grass", gr$version[ind]),
+#                      no = gr$instDir)
 
 
 
