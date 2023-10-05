4664a6b77012bcf2810c80370d5978bf420574ca
Beaudette <dylan.beaudette@usda.gov>
2023-10-05 13:58:44 -0700

    a couple more figures

--- eval-corestrictive-features.R
+++ eval-corestrictive-features.R
@@ -1,4 +1,5 @@
 library(aqp)
+library(ragg)
 
 ## combined mu/component data, as SPC
 x <- readRDS('data/combined-tab-data-SPC.rds')
@@ -20,12 +21,15 @@ table(x.u$source)
 
 
 # SSURGO
-x.sub <- subset(x.u, source == 'SSURGO')[1:20, ]
+x.sub <- subset(x.u, source == 'SSURGO')
 length(x.sub)
 
 par(mar = c(0, 0, 3, 1))
 plotSPC(x.sub, label = 'compname', name.style = 'center-center', width = 0.35, depth.axis = list(style = 'compact', line = -3, cex = 0.8), col.label = 'Texture Class (<2mm fraction)', color = 'texture')
 
+## TODO: split over 2 rows
+
+agg_png(filename = 'figures/SSURGO-unique-comp-rest-features-sketches.png', width = 2800, height = 650, scaling = 1.5)
 
 par(mar = c(0, 0, 3, 1))
 plotSPC(x.sub, label = 'compname', id.style = 'top', cex.id = 0.66, cex.names = 0.85, name.style = 'center-center', width = 0.35, depth.axis = list(style = 'compact', line = -3, cex = 0.8), col.label = 'Texture Class (<2mm fraction)', color = 'texture', col.palette = hcl.colors(10, 'vik', rev = TRUE))
@@ -37,15 +41,19 @@ addDiagnosticBracket(x.sub, kind = 'Paralithic bedrock', feature = 'reskind', to
 addDiagnosticBracket(x.sub, kind = 'Strongly contrasting textural stratification', feature = 'reskind', top = 'resdept_r',  bottom = 'resdepb_r', offset = -0.5, col = 4, tick.length = 0, lwd = 4)
 
 legend('topright', legend = c('lithic contact', 'paralithic', 'strongly contrasting textural change'), cex = 0.8, bty = 'n', lwd = 3, col = 2:4, xpd = NA, inset = c(0, -0.06))
+
+dev.off()
 
 
 # RSS
-x.sub <- subset(x.u, source == 'RSS')[1:20, ]
+x.sub <- subset(x.u, source == 'RSS')
 length(x.sub)
 
 par(mar = c(0, 0, 3, 1))
 plotSPC(x.sub, label = 'compname', name.style = 'center-center', width = 0.35, depth.axis = list(style = 'compact', line = -3, cex = 0.8), col.label = 'Texture Class (<2mm fraction)', color = 'texture')
 
+
+agg_png(filename = 'figures/RSS-unique-comp-rest-features-sketches.png', width = 1800, height = 650, scaling = 1.5)
 
 par(mar = c(0, 0, 3, 1))
 plotSPC(x.sub, label = 'compname', id.style = 'top', cex.id = 0.66, cex.names = 0.85, name.style = 'center-center', width = 0.35, depth.axis = list(style = 'compact', line = -3, cex = 0.8), col.label = 'Texture Class (<2mm fraction)', color = 'texture', col.palette = hcl.colors(10, 'vik', rev = TRUE))
@@ -58,4 +66,4 @@ addDiagnosticBracket(x.sub, kind = 'Strongly contrasting textural stratification
 
 legend('topright', legend = c('lithic contact', 'paralithic', 'strongly contrasting textural change'), cex = 0.8, bty = 'n', lwd = 3, col = 2:4, xpd = NA, inset = c(0, -0.06))
 
-
+dev.off()
