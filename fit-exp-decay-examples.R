44c5a97350454993f9d123c05a53fe141a1447f1
Beaudette <dylan.beaudette@usda.gov>
2023-10-18 09:07:21 -0700

    Update fit-exp-decay-examples.R

--- fit-exp-decay-examples.R
+++ fit-exp-decay-examples.R
@@ -16,6 +16,45 @@ p <- predict(n, newdata = data.frame(z = 1:150))
 lines(1:150, p, col = 2)
 
 
+.f <- function(z, Ksat_0, r) {
+  Ksat_0 * exp(-r * z)
+}
+
+plot(1:150, .f(1:150, Ksat_0 = 80, r = 0.05), las = 1)
+
+
+d <- data.frame(
+  z = seq(0, 150, length.out = 25)
+)
+
+d$ksat <- pmax(.f(d$z, Ksat_0 = 80, r = 0.05) + rnorm(n = 25, mean = 0, sd = 3), 0.01)
+
+plot(ksat ~ z, data = d, type = 'b', las = 1)
+
+
+d$logksat <- log(d$ksat)
+plot(logksat ~ z, data = d, type = 'p', las = 1, cex = 0.5, pch = 16)
+
+l <- lm(logksat ~ z, data = d)
+summary(l)
+coef(l)
+exp(coef(l)[1])
+
+
+plot(ksat ~ z, data = d, type = 'b', las = 1)
+p <- exp(coef(l)[1]) * exp(coef(l)[2] * d$z)
+lines(d$z, p, col = 2)
+
+
+nls(ksat ~ SSasymp(z, Asym, R0, lrc), data = d)
+pp <- predict(n, newdata = data.frame(z = d$z))
+lines(d$z, pp, col = 3)
+
+
+
+
+
+
 d <- dice(x[5, ], ~ ksat.rosetta, SPC = FALSE)
 plot(ksat.rosetta ~ hzdept_r, data = d, type = 'p', las = 1, cex = 0.5, pch = 16)
 
@@ -32,8 +71,3 @@ plot(logksat ~ hzdept_r, data = d, type = 'p', las = 1, cex = 0.5, pch = 16)
 l <- lm(logksat ~ hzdept_r, data = d)
 summary(l)
 coef(l)
-
-plot(ksat.rosetta ~ hzdept_r, data = d, type = 'p', las = 1, cex = 0.5, pch = 16)
-
-p <- exp(coef(l)[1]) * exp(-coef(l)[2] * 1:200)
-lines(1:200, p, col = p)
