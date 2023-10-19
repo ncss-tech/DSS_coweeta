9b90c1ea43941499f7719749c7d75ed5fb09b0bd
Beaudette <dylan.beaudette@usda.gov>
2023-10-19 12:13:43 -0700

    Update eval-model-calibration.R

--- eval-model-calibration.R
+++ eval-model-calibration.R
@@ -4,9 +4,13 @@ library(tactile)
 s <- read.table('calibration-eval/calibrationoutputssurgo.txt', header = TRUE)
 d <- read.table('calibration-eval/calibrationoutputdsm.txt', header = TRUE)
 
-x <- make.groups(ssurgo = s, rss = d)
+x <- make.groups(SSURGO = s, `Raster Soil Survey` = d)
 
-wilcox.test(s$lnNSE, d$lnNSE)
+## maybe not all that informative as compared with the distributions themselves
+# wilcox.test(lnNSE ~ which, data = x)
+
+# interesting
+bwplot(which ~ NSE, data = x, par.settings = tactile.theme(), notch = TRUE)
 
 bwplot(which ~ lnNSE, data = x, par.settings = tactile.theme(), notch = TRUE)
 bwplot(which ~ KGE, data = x, par.settings = tactile.theme(), notch = TRUE)
